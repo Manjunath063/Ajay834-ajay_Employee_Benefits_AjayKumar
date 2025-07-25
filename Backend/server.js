e0bf5048374c206e1e6dd9173673bba1732992fc
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-25 11:31:54 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -72,13 +72,13 @@ const upload = multer({
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    "http://16.171.196.69:3422",
+    "http://16.171.39.126:3422",
     "http://127.0.0.1:5500",
-    "http://16.171.196.69:5500",
+    "http://16.171.39.126:5500",
     "http://127.0.0.1:5501",
     "http://127.0.0.1:5503",
-    "http://16.171.196.69:8043",
-    "http://16.171.196.69:8044",
+    "http://16.171.39.126:8043",
+    "http://16.171.39.126:8044",
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -238,7 +238,7 @@ app.get('/hr', (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on http://16.171.196.69:${port}`);
+  console.log(`Server running on http://16.171.39.126:${port}`);
 });
 
 process.on('SIGINT', () => {
